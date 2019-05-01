29a84cec1f57a9acb476538cad5ebae1952fd56b
CodeBuzz <codebuzz.net@gmail.com>
2019-05-01 19:51:47 -0400

    premitive obsession

--- OrderService/Controllers/ValuesController.cs
+++ OrderService/Controllers/ValuesController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using OrderService1;
+using S2;
+
 
 namespace OrderService.Controllers
 {
@@ -12,22 +15,68 @@ namespace OrderService.Controllers
     {
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<Employee> Get(int id)
         {
-            return new string[] { "value1", "value2" };
+            var contact = new OrderService1.Contact("123-456-6789");
+            var areaCode = contact.GetAreaCode();
+            OrderService1.Contact contact1 = new OrderService1.Contact("123-456-7890");
+            SocialSecurity ssn = new SocialSecurity("111-22-3333");
+
+            Employee emp = new Employee("abcd", "asdas", contact1, ssn);
+
+            return emp;
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public ActionResult<Employees> Get()
         {
-            return "value";
+            
+            var phone = "111-22-3333";
+            var areacode = string.Empty;
+            Employees emp = null;
+            try
+            {
+                // Get area code from phone number
+                int index = phone.IndexOf("-", StringComparison.Ordinal);
+                if (index > 0 && index < phone.Length)
+                    areacode = phone.Substring(0,index);
+
+                // Business logic
+                emp = new Employees("abcd", "asdas", areacode, phone);
+                emp.GetEmployeeDetails();
+                if (emp == null)
+                { return NoContent(); }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+            // Business logic
+
+            return Ok(emp);
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody]Employee value)
         {
+           if(value!=null)
+            {
+                string employeeAreaCode= value.Contact.GetAreaCode();
+                //string employee4DigitSSN = value.SocialSecurity.GetLast4Digit();
+            }
+        }
+
+        [HttpPost]
+        [Route("ssn")]
+        public void Update([FromBody]Employee value)
+        {
+            if (value != null)
+            {
+                string employee4DigitSSN = value.SocialSecurity.GetLast4Digit();
+            }
         }
 
         // PUT api/values/5
---
+++ OrderService/Order.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace S2
+{
+
+    public class Order
+    {
+        public Order(string name, string email, int orderid, string address)
+        {
+            CustomerName = name;
+            CustomerEmail = email;
+            OrderId = orderid;
+            Address = address;
+        }
+        public string CustomerName { get; private set; }
+        public string CustomerEmail { get; private set; }
+        public int OrderId { get; set; }
+        public string Address { get; set; }
+    }
+
+    public class Employees
+    {
+        public Employees(string firstName, string lastName,string contactCellNo,string ssn)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            PhoneNumber = contactCellNo;
+            SSN = ssn;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string SSN { get; set; }
+        public string GetAreaCode()
+        {
+            var phone = PhoneNumber;
+            // Extract the area code
+            var areacode = "";
+            int index = phone.LastIndexOf("-", StringComparison.Ordinal);
+            if (index > 0 && index < phone.Length)
+                areacode = phone.Substring(0, index);
+            return areacode;
+        }
+
+        internal void GetEmployeeDetails()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetLast4Digit()
+        {
+            var index = SSN.LastIndexOf("-", StringComparison.Ordinal);
+            return index > 0 && index < SSN.Length
+                ? SSN.Substring(index + 1, SSN.Length - index + 1)
+                : SSN;
+        }
+    }
+
+    public class Contact
+    {
+        public Contact(string number)
+        {
+            if (string.IsNullOrWhiteSpace(number))
+                throw new InvalidDataException();
+
+            PhoneNumber = number;
+        }
+
+        public string PhoneNumber { get; }
+
+        public string GetAreaCode()
+        {
+            var index = PhoneNumber.IndexOf("-", StringComparison.Ordinal);
+            return index > 0 && index < PhoneNumber.Length
+                ? PhoneNumber.Substring(0,index)
+                : PhoneNumber;
+        }
+    }
+}
+
+namespace OrderService1
+{
+    public class Order
+    {
+        public Order(string customerName, string customerEmail, int orderId, string orderDate, string address, string city, string state, int orderAmout)
+        {
+            CustomerName = customerName;
+            CustomerEmail = customerEmail;
+            OrderId = orderId;
+            OrderDate = orderDate;
+            Address = address;
+            City = city;
+            State = state;
+            OrderAmout = orderAmout;
+        }
+
+        public string CustomerName { get; private set; }
+        public string CustomerEmail { get; private set; }
+        public int OrderId { get; set; }
+        public string OrderDate { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int OrderAmout { get; set; }
+        public DateTime date { get; set; }
+
+       
+    }
+
+    public class Employee
+    {
+        public Employee(string firstName, string lastName,Contact contactCellNo, SocialSecurity ssn)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Contact = contactCellNo;
+            SocialSecurity = ssn;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Contact Contact { get; set; }
+        public SocialSecurity SocialSecurity { get; set; }
+    }
+
+    public class SocialSecurity
+    {
+        public SocialSecurity(string ssn)
+        {
+            SSN = ssn;
+        }
+
+        public string SSN { get; set; }
+
+        public string GetLast4Digit()
+        {
+            var index = SSN.LastIndexOf("-", StringComparison.Ordinal);
+            return index > 0 && index < SSN.Length
+                ? SSN.Substring(index+1, SSN.Length -index+1)
+                : SSN;
+        }
+    }
+
+    public class Contact
+    {
+        public Contact(string number)
+        { 
+            PhoneNumber = number;
+        }
+
+        public string PhoneNumber { get; set; }
+
+        public string GetAreaCode()
+        {
+            var index = PhoneNumber.IndexOf("-", StringComparison.Ordinal);
+            return index > 0 && index < PhoneNumber.Length
+                ? PhoneNumber.Substring(0, index)
+                : PhoneNumber;
+        }
+    }
+
+}
